ff86cc1efbcc24f37e230a2daa8b60e7f4bd6fac
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2023-03-23 12:43:58 -0300

    fix: change column price on table products to Int

--- src/repositories/cartsRepository.ts
+++ src/repositories/cartsRepository.ts
@@ -2,13 +2,34 @@ import prisma from "../config/database";
 import { CreateCartData } from "../services/cartsService";
 
 async function addToCart(newCart: CreateCartData) {
-  await prisma.carts.create({
-    data: newCart
+  await prisma.carts.create({data: newCart})
+}
+
+async function getUserCart(userId: number) {
+  const carts = prisma.carts.findMany({
+    where: {
+      userId
+    },
+    include: {
+      products: {
+        select: {
+          name: true,
+          price: true,
+          description: true,
+          image: true,
+          size: true,
+          units: true
+        }
+      }
+    }
   })
+
+  return carts
+} 
+
+const cartsRepository = {
+  addToCart,
+  getUserCart
 }
 
-const cartRepository = {
-  addToCart
-}
-
-export default cartRepository
+export default cartsRepository
--- src/services/cartsService.ts
+++ src/services/cartsService.ts
@@ -1,3 +1,17 @@
 import { carts } from "@prisma/client";
 
+import cartsRepository from "../repositories/cartsRepository";
+
 export type CreateCartData = Omit<carts, "id">
+
+async function addToCart(newCart: CreateCartData) {
+  await cartsRepository.addToCart(newCart)
+}
+
+async function getUserCart(userId: number) {
+  const cart = await cartsRepository.getUserCart(userId)
+
+  cart.forEach((product) => {
+    //product.products.price = product.products.price * product.products.units
+  })
+}
